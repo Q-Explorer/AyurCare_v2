d92676409304b995753d93338bf37478881c5d2a
Error404 <237201771+Q-Explorer@users.noreply.github.com>
2026-05-04 20:46:17 +0530

    Delete AyurCare_C++_with_GUI_and_exception_handling/run.bat

--- AyurCare_C++_with_GUI_and_exception_handling/run.bat
+++
@@ -1,5 +0,0 @@
-g++ AyurCare_GUI.cpp imgui/imgui.cpp imgui/imgui_draw.cpp imgui/imgui_tables.cpp imgui/imgui_widgets.cpp imgui/backends/imgui_impl_glfw.cpp imgui/backends/imgui_impl_opengl3.cpp -I. -Iimgui -Iimgui/backends -L. -lglfw3 -lopengl32 -lgdi32 -o AyurCare.exe
-
-AyurCare.exe
-
-pause
